fee8599e8ca338ca5dff6a32162aaec1681de1fe
Xajper <104763972+xajper@users.noreply.github.com>
2024-10-26 13:14:10 +0200

    Update generatorHasel.js

--- generatorHasel.js
+++ generatorHasel.js
@@ -1,27 +1,90 @@
-function generatePassword() {
-    const passwordLength = 12;
-    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    let password = "";
+const lengthSlider = document.querySelector(".pass-length input");
+const options = document.querySelectorAll(".option input");
+const copyIcon = document.querySelector(".input-box span");
+const passwordInput = document.querySelector(".input-box input");
+const passIndicator = document.querySelector(".pass-indicator");
+const generateBtn = document.querySelector(".generate-btn");
 
-    for (let i = 0; i < passwordLength; i++) {
-        const randomIndex = Math.floor(Math.random() * charset.length);
-        password += charset[randomIndex];
-    }
-
-    document.getElementById("password").value = password;
+const characters = {
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers: "0123456789",
+    symbols: "!$%&|[](){}:;.,*+-#@<>~"
 }
 
-function copyPassword() {
-    const passwordField = document.getElementById("password");
-    passwordField.select();
-    
-    try {
-        document.execCommand("copy");
-        setTimeout(() => {
-            document.querySelector('.containerkopiuj input').checked = false;
-        }, 1000);
-    } catch (err) {
-        console.error("Błąd kopiowania do schowka: ", err);
-        alert("Niestety, nie udało się skopiować do schowka.");
+const generatePassword = () => {
+    let staticPassword = "",
+        randomPassword = "",
+        excludeDuplicate = false,
+        passLength = lengthSlider.value;
+
+    options.forEach(option => {
+        if (option.checked) {
+            if (option.id !== "exc-duplicate" && option.id !== "spaces") {
+                staticPassword += characters[option.id];
+            } else if (option.id === "spaces") {
+                staticPassword += `  ${staticPassword}  `;
+            } else {
+                excludeDuplicate = true;
+            }
+        }
+    });
+
+    for (let i = 0; i < passLength; i++) {
+        let randomChar = staticPassword[Math.floor(Math.random() * staticPassword.length)];
+        if (excludeDuplicate) {
+            !randomPassword.includes(randomChar) || randomChar == " " ? randomPassword += randomChar : i--;
+        } else {
+            randomPassword += randomChar;
+        }
+    }
+    passwordInput.value = randomPassword;
+
+}
+
+const updatePassIndicator = () => {
+    passIndicator.id = lengthSlider.value <= 8 ? "weak" : lengthSlider.value <= 16 ? "medium" : "strong";
+}
+
+const updateSlider = () => {
+    document.querySelector(".pass-length span").innerText = lengthSlider.value;
+    generatePassword();
+    updatePassIndicator();
+}
+updateSlider();
+
+const copyPassword = () => {
+    navigator.clipboard.writeText(passwordInput.value);
+    copyIcon.innerText = "check";
+    copyIcon.style.color = "#4285f4";
+    setTimeout(() => {
+        copyIcon.innerText = "copy_all";
+        copyIcon.style.color = "#707070";
+    }, 1500);
+}
+
+copyIcon.addEventListener("click", copyPassword);
+lengthSlider.addEventListener("input", updateSlider);
+generateBtn.addEventListener("click", generatePassword);
+
+function blokujMysz(event) {
+    console.log("Kliknięcie: ", event.button);
+    if (event.button === 2 || event.which === 3) {
+        event.preventDefault();
     }
 }
+  
+function blokujKlawisze(event) {
+    if (event.key === 'F12') {
+        event.preventDefault();
+    }
+  
+    if (event.ctrlKey && event.key === 'u') {
+        event.preventDefault();
+    }
+}
+  
+document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('mousedown', blokujMysz);
+    document.addEventListener('keydown', blokujKlawisze);
+});
