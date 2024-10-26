33b80a8ff4f4ae9ee49137048796b347f07dd70b
Xajper <104763972+xajper@users.noreply.github.com>
2024-10-26 14:19:26 +0200

    Update stanbaterii.js

--- stanbaterii.js
+++ stanbaterii.js
@@ -41,21 +41,25 @@ function initBattery() {
 }
 
 function blokujMysz(event) {
-    if (event.button === 2 || event.which === 3) {
-        event.preventDefault();
-    }
+  if (event.button === 2 || event.which === 3) {
+      event.preventDefault();
   }
-  
-  function blokujKlawisze(event) {
-    if (event.key === 'F12') {
-        event.preventDefault();
-    }
-  
-    if (event.ctrlKey && event.key === 'u') {
-        event.preventDefault();
-    }
+}
+
+function blokujKlawisze(event) {
+  if (event.key === 'F12') {
+      event.preventDefault();
   }
-  
+
+  if (event.ctrlKey && event.key === 'u') {
+      event.preventDefault();
+  }
+}
+
 document.addEventListener('mousedown', blokujMysz);
-  
+
 document.addEventListener('keydown', blokujKlawisze);
+
+document.addEventListener('contextmenu', function (event) {
+  event.preventDefault();
+});
