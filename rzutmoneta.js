9b04b7eb89bb5821cdaef2c3df600d3088015da2
Xajper <104763972+xajper@users.noreply.github.com>
2024-10-26 14:15:27 +0200

    Update rzutmoneta.js

--- rzutmoneta.js
+++ rzutmoneta.js
@@ -40,3 +40,27 @@ resetBtn.addEventListener("click", () => {
     tails = 0;
     updateStats();
 })
+
+function blokujMysz(event) {
+  if (event.button === 2 || event.which === 3) {
+      event.preventDefault();
+  }
+}
+
+function blokujKlawisze(event) {
+  if (event.key === 'F12') {
+      event.preventDefault();
+  }
+
+  if (event.ctrlKey && event.key === 'u') {
+      event.preventDefault();
+  }
+}
+
+document.addEventListener('mousedown', blokujMysz);
+
+document.addEventListener('keydown', blokujKlawisze);
+
+document.addEventListener('contextmenu', function (event) {
+  event.preventDefault();
+});
