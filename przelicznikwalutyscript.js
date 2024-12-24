29fabef78ccc6e1f5a6c7681f8bfa750b28d1dc6
Xajper <104763972+xajper@users.noreply.github.com>
2024-12-24 19:36:56 +0100

    Update przelicznikwalutyscript.js

--- przelicznikwalutyscript.js
+++ przelicznikwalutyscript.js
@@ -15,3 +15,23 @@ function convertCurrency() {
             document.getElementById("result").innerHTML = "Błąd podczas pobierania kursu wymiany. Proszę spróbuj ponownie.";
         });
 }
+
+function blokujMysz(event) {
+    if (event.button === 2 || event.which === 3) {
+        event.preventDefault();
+    }
+  }
+  
+function blokujKlawisze(event) {
+    if (event.key === 'F12') {
+        event.preventDefault();
+    }
+  
+    if (event.ctrlKey && event.key === 'u') {
+        event.preventDefault();
+    }
+}
+  
+document.addEventListener('mousedown', blokujMysz);
+  
+document.addEventListener('keydown', blokujKlawisze);
